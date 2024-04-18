6cfe1d6258c37a0ab83a2920c087f54074d5e30c
hakanyalniz <hakan_yalniz@live.com>
2024-04-18 23:38:50 +0300

    Used styled components to redo the CSS. Used as a basic tutorial.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
 import silverWolfLogo from "./assets/silverwolf-icon.png";
-import "./App.css";
+import styled, { keyframes } from "styled-components";
+
+// import "./App.css";
+
+const logoSpin = keyframes`
+from {
+  transform: rotate(0deg);
+}
+to {
+  transform: rotate(360deg);
+}
+`;
+
+const Logo = styled.img`
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+
+  &:hover {
+    filter: drop-shadow(0 0 2em #9a2dffaa);
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    &:nth-of-type(1) {
+      animation: ${logoSpin} infinite 20s linear;
+    }
+  }
+`;
 
 function App() {
   return (
@@ -8,8 +36,10 @@ function App() {
         <a
           href="https://honkai-star-rail.fandom.com/wiki/Silver_Wolf"
           target="_blank"
+          rel="noreferrer"
         >
-          <img src={silverWolfLogo} className="logo" alt="Vite logo" />
+          <Logo src={silverWolfLogo} alt="Vite logo"></Logo>
+          {/* <img src={silverWolfLogo} className="logo" alt="Vite logo" /> */}
         </a>
       </div>
       <h1>Vite + React</h1>
--- src/StateExample.jsx
+++ src/StateExample.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
-import "./StateExample.css";
+import { useState } from "react";
+import styled from "styled-components";
+
+// import "./StateExample.css";
 
 const COLORS = ["pink", "green", "blue", "yellow", "purple"];
 
@@ -17,27 +19,67 @@ function StateExample() {
   };
 
   return (
-    <>
-      <div
-        className="StateExample"
+    <FlexContainer>
+      <ExampleForState
         style={{
           backgroundColor,
         }}
       >
         {COLORS.map((color) => (
-          <button
+          <Button
             type="button"
             key={color}
             onClick={onButtonClick(color)}
             className={backgroundColor === color ? "selected" : ""}
           >
             {color}
-          </button>
+          </Button>
         ))}
-      </div>
-      <div className="counter">{clickCount}</div>
-    </>
+      </ExampleForState>
+      <Counter>{clickCount}</Counter>
+    </FlexContainer>
   );
 }
+
+const FlexContainer = styled.div`
+  margin: auto;
+  width: 90%;
+`;
+
+const ExampleForState = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-wrap: wrap;
+  height: 50vh;
+  transition: all 0.1s ease-in;
+`;
+
+const Button = styled.button`
+  color: black;
+  padding: 1rem 2rem;
+  font-size: 1.5rem;
+  font-weight: 600;
+  border: none;
+  border-radius: 0.5rem;
+  background-color: #f1f1f1;
+  cursor: pointer;
+  transition: all 0.1s ease-in-out;
+  box-shadow: 0px 1px 10px 0px rgba(0, 0, 0, 0.5);
+
+  &:hover {
+    box-shadow: 0px 5px 10px 0px rgba(0, 0, 0, 0.5);
+    background-color: #fff;
+  }
+
+  &.selected {
+    outline: 5px solid #000;
+    background-color: #fff;
+  }
+`;
+
+const Counter = styled.div`
+  font-size: 2rem;
+`;
 
 export default StateExample;
